33bf88ce00fcc7cb71778ce0ce45c3a2783c84cd
trapperdot00 <csernusricsike@gmail.com>
2025-10-10 00:06:49 +0200

    Modified test program to take its arguments to minesweeper from the command-line.

--- test/0001/main.cpp
+++ test/0001/main.cpp
@@ -1,6 +1,36 @@
 #include "minesweeper.h"
 
-int main() {
-	minesweeper ms{10, 10, 25};
+#include <stdexcept>
+#include <string>
+#include <cstddef>
+#include <iostream>
+
+struct usage_error : std::exception {
+	const char* what() const noexcept {
+		return "usage: <program> <width> <height> <mines>";
+	}
+};
+
+struct cli_args {
+	cli_args(int argc, char* argv[]) try {
+		if (argc < 4) {
+			throw usage_error{};
+		}
+		width = std::stoull(argv[1]);
+		height = std::stoull(argv[2]);
+		mines = std::stoull(argv[3]);
+	} catch (...) {
+		throw usage_error{};
+	}
+	size_t width;
+	size_t height;
+	size_t mines;
+};
+
+int main(int argc, char* argv[]) try {
+	cli_args args{argc, argv};
+	minesweeper ms{args.width, args.height, args.mines};
 	ms.play();
+} catch (const std::exception& err) {
+	std::cerr << err.what() << '\n';
 }
